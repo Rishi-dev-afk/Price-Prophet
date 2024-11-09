bc8192dfda09a79783949dee26e73c4ee39536b2
Rishi-dev-afk <nayakbibhesh1@gmail.com>
2024-11-09 10:56:02 +0530

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+
+def load_and_clean_data(stock_symbol, start_date, end_date):
+    data = yf.download(stock_symbol, start=start_date, end=end_date)
+    data = data[['Close']]  # Only keep closing prices
+    data.dropna(inplace=True)
+    
+    # Normalize data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data['Close'] = scaler.fit_transform(data[['Close']])
+    
+    return data, scaler
 
